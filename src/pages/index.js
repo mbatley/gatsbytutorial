30a035b2705ec4031b52435422a357ceb28bca78
mbatley <msbcoder@gmail.com>
2022-01-25 16:40:12 +0000

    Finished Gatsby Tutorial Part 7

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
             <p>I'm making this by following the Gatsby Tutorial.</p>
             <StaticImage
                 alt="IG Feesable Logo"
-                src="../images/feesable.gif"
+                src="../images/feesable.jpg"
             />
         </Layout>
     )
